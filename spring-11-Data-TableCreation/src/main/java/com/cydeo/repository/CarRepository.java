75ff47cbe11b2e495202806a682d458e62a23e4f
francis-d2020 <francis_d95@hotmail.>
2024-10-02 20:09:41 +0300

    in this module, I studied to crete relaions between tables and the different types of relations.

--- spring-11-Data-TableCreation/src/main/java/com/cydeo/repository/CarRepository.java
+++ spring-11-Data-TableCreation/src/main/java/com/cydeo/repository/CarRepository.java
@@ -5,8 +5,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository //this determines the interface as services so that in execution time
-//spring boot creates implementation of the interface
-//niw this annotation not necessary
+//spring boot creates implementation class of the interface
+//now this annotation not necessary
 //by this extending we get the methods that are related to DB from jpaRepository
 public interface CarRepository extends JpaRepository<Car,Long> {
 
