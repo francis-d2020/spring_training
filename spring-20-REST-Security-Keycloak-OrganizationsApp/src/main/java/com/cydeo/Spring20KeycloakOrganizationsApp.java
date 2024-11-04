019d0669d62d36ff250c7760488418e6a2a3ce2e
francis-d2020 <francis_d95@hotmail.>
2024-11-04 20:16:26 +0200

    In module 20 License and Organization i created a realm, client users, roles in keycloak. oath2 security to get tokens so certain roles can do available actions by the role.

---
+++ spring-20-REST-Security-Keycloak-OrganizationsApp/src/main/java/com/cydeo/Spring20KeycloakOrganizationsApp.java
@@ -0,0 +1,13 @@
+package com.cydeo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring20KeycloakOrganizationsApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring20KeycloakOrganizationsApp.class, args);
+    }
+
+}
